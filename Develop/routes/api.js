d7d4ef6fdef16ab92535746b13a5a294abe328ba
Jordan Shipley <jordan.j.shipley@gmail.com>
2021-01-23 22:11:31 -0700

    updating routes

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -1,14 +1,27 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
+
+//Create new workouts
+router.post("/api/workouts", function(req, res) {
+    Workout.create({})
+    .then(dbWorkout =>{
+        res.json(dbWorkout);
+    })
+    .catch(err =>{
+        res.status(400).json(err)
+    })
+});
+
+
 //GET workouts
 router.get("/api/workouts", function(req, res){
     Workout.find()
-        .then(data =>{
-            res.json(data)
+        .then(dbWorkout =>{
+            res.json(dbWorkout)
         })
         .catch(err=>{
-            res.json(err)
+            res.status(400).json(err)
         })
 });
-
+//PUT
